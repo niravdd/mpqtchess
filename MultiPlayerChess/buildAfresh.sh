25ade1d97b56dd64c402bcf803e0620b51ba7ca0
Nirav Doshi <3119359+niravdd@users.noreply.github.com>
2025-10-03 18:36:04 +1000

    Updated code...

--- MultiPlayerChess/buildAfresh.sh
+++ MultiPlayerChess/buildAfresh.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Build started: $(date '+%Y-%m-%d %H:%M:%S')"
+
 rm -rf build
 mkdir build
 cd build
 cmake .. -DCMAKE_PREFIX_PATH=/Users/niravdd/Qt/6.5.3/macos
 cmake --build .
+
+echo "Build completed: $(date '+%Y-%m-%d %H:%M:%S')"
